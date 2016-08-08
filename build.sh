5a69eda1dc67181f452ca4451143e69cf6db12ea
Avi Cavale <avi@shippable.com>
2016-08-08 13:17:58 -0700

    adding new build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,47 @@
 #!/bin/bash -e
-if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker build -t $IMAGE_NAME:$BRANCH.$BUILD_NUMBER .
-else
-  echo "skipping because it's a PR"
-fi
+
+export BRANCH=master
+export IMAGE_NAME=avinci/box
+export IMAGE_TAG=$BRANCH.$BUILD_NUMBER
+export RES_DOCKER_CREDS=docker-creds
+export RES_DOCKER_CREDS_INT=avinci-dh.json
+export RES_BOX_REPO=box-repo
+export RES_BOX_IMAGE=box-img
+
+dockerBuild() {
+  echo "Starting Docker build for" $IMAGE_NAME:$IMAGE_TAG
+  cd ./IN/$RES_BOX_REPO/$RES_BOX_REPO
+  sudo docker build -t=$IMAGE_NAME:$IMAGE_TAG .
+  echo "Completed Docker build for" $IMAGE_NAME:$IMAGE_TAG
+}
+
+dockerPush() {
+  echo "Starting Docker push for" $IMAGE_NAME:$IMAGE_TAG
+  sudo docker push $IMAGE_NAME:$IMAGE_TAG
+  echo "Completed Docker push for" $IMAGE_NAME:$IMAGE_TAG
+}
+
+dockerLogin() {
+  echo "Extracting docker creds"
+  cat ./IN/$RES_DOCKER_CREDS/$RES_DOCKER_CREDS_INT  | jq -r '.formJSONValues | map(.label + "=" + .value)|.[]' > dockerInt.sh
+  . dockerInt.sh
+  echo "logging into Docker with username" $username
+  docker login -u $username -p $password
+  echo "Completed Docker login"
+}
+
+createOutState() {
+  echo "Creating a state file for" $RES_BOX_IMAGE
+  echo versionName=$IMAGE_TAG > /build/state/$RES_BOX_IMAGE.env
+  cat /build/state/$RES_BOX_IMAGE.env
+  echo "Completed creating a state file for" $RES_BOX_IMAGE
+}
+
+main() {
+  dockerLogin
+  dockerBuild
+  dockerPush
+  createOutState
+}
+
+main
